f03fdbfbd16a609ae7e46165669d491e6cc204fe
Mauro Cordoba <mauro61816@gmail.com>
2022-08-27 11:15:14 -0300

    Javascript program prediction
    Program for predict points of slots streaming and win profits in cash only farming hours

--- slots.js
+++ slots.js
@@ -1,5 +1,8 @@
 const points = '';
-let btc22 = Math.round((22 / 45000) * 10000);
+
+//----MOST COEFICIENTE IS BETTER PROFIT FOR THE POINS
+
+let btc22 = (22 / 45000) * 10000;
 let btc25 = (25 / 50000) * 10000;
 let btc50 = (50 / 75000) * 10000;
 let btc69 = (69 / 95000) * 10000;
@@ -9,16 +12,17 @@ let btc420 = (420 / 400000) * 10000;
 let btc500 = (500 / 475000) * 10000;
 let btc1000 = (1000 / 725000) * 10000;
 
-document.write(btc22 + '</br>');
+// ------------------- //
 
 let profitsBtc =[];
 profitsBtc.push(btc22,btc25,btc50,btc69,btc100,btc250,btc500,btc1000);
 
-profitsBtc.sort();
+for ( let i = 0; i < 7; i++) {
+    document.write(profitsBtc[i]+ '</br>');
+}
 
 
-document.write(profitsBtc+ '</br>');
 
-for (let i = 0; i < profitsBtc.length; i++) {
-    document.write(profitsBtc[i]);
+const listBtc = (btc) => {
+    coeficient = []
 }

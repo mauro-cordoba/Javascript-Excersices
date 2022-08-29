d5141644b8ae3f8e4468805a0b986a772cff737e
Mauro Cordoba <mauro61816@gmail.com>
2022-08-28 21:49:25 -0300

    Program predict roulette
    Javascript prototype

--- rulet.js
+++ rulet.js
@@ -1,30 +1,66 @@
 
-const roulette = (n) => {
+// const roulette = (n) => {
+// 		for (let i = 0; i < n; i++) {
+// 			let start = 0;
+// 			let end = 14;
+// 			ball = Math.floor(Math.random() * (end - start + 1)) + start;
+// 			if ( ball % 2 == 0 && ball != 0){
+// 				let $colorBlack = "BLACK";
+// 				// document.write(`Nº${ball}<b> ${$colorBlack}</b></br></br>`);
+// 				return $colorBlack;
+// 			}
+// 			if ( ball % 2 != 0){
+// 				let $colorRed = "RED";
+// 				// document.write(`Nª ${ball}<b style='color:red'> ${$colorRed}</b></br></br>`);
+// 				return $colorRed;
+// 			}
+// 			if ( ball == 0) {
+// 				let $colorGreen = "GREEN";
+// 				// document.write(`Nª ${ball}<b style='color:green'> ${$colorGreen}</b></br></br>`);
+// 				return $colorGreen;
+// 			}
+// 		}
+// }
+
+// myColor = prompt('Insert a COLOR')
+
+const predictor = (myColor, n) => {
+
+	for (let i = 0; i < n; i++) {
+
+	myColor = myColor.toUpperCase();
+
+	const turn = (n) => {
 		for (let i = 0; i < n; i++) {
 			let start = 0;
 			let end = 14;
 			ball = Math.floor(Math.random() * (end - start + 1)) + start;
 			if ( ball % 2 == 0 && ball != 0){
-				let $color = 'BLACK';
-				document.write(`Nº${ball}<b> BLACK</b></br></br>`);
-				return $color;
+				let $colorBlack = "BLACK";
+				// document.write(`Nº${ball}<b> ${$colorBlack}</b></br></br>`);
+				return $colorBlack;
 			}
 			if ( ball % 2 != 0){
-				let $color = 'RED';
-				document.write(`Nª ${ball}<b style='color:red'> RED</b></br></br>`);
-				return $color;
+				let $colorRed = "RED";
+				// document.write(`Nª ${ball}<b style='color:red'> ${$colorRed}</b></br></br>`);
+				return $colorRed;
 			}
 			if ( ball == 0) {
-				let $color = 'GREEN';
-				document.write(`Nª ${ball}<b style='color:green'> ZERO</b></br></br>`);
-				return $color;
+				let $colorGreen = "GREEN";
+				// document.write(`Nª ${ball}<b style='color:green'> ${$colorGreen}</b></br></br>`);
+				return $colorGreen;
 			}
 		}
 }
-
-roulette(14);
-
-let number = prompt('Insert a COLOR');
-const predictor = (number) => {
-	
+		if ( myColor == turn ) {
+			document.write(`BALL: ${turn}</br><b style='color:gold'>You WIN</b>'</br>'`)
+		}
+		if ( myColor != turn || myColor != turn || myColor != turn) {
+			document.write(`BALL: ${turn}</br><b style='color:grey'>You LOSE</b>'</br>'`)
+		}
+	}
 }
+
+predictor('BLACK', 12);
+
+

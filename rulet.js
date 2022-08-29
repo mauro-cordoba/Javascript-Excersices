ed9ebed13310ef5c46f074decf9fc110016d28d2
Mauro Cordoba <mauro61816@gmail.com>
2022-08-28 21:08:05 -0300

    Prediction by Javascript
    Roullete color random program

--- rulet.js
+++ rulet.js
@@ -4,18 +4,27 @@ const roulette = (n) => {
 			let start = 0;
 			let end = 14;
 			ball = Math.floor(Math.random() * (end - start + 1)) + start;
-
 			if ( ball % 2 == 0 && ball != 0){
+				let $color = 'BLACK';
 				document.write(`Nº${ball}<b> BLACK</b></br></br>`);
+				return $color;
 			}
 			if ( ball % 2 != 0){
+				let $color = 'RED';
 				document.write(`Nª ${ball}<b style='color:red'> RED</b></br></br>`);
+				return $color;
 			}
 			if ( ball == 0) {
+				let $color = 'GREEN';
 				document.write(`Nª ${ball}<b style='color:green'> ZERO</b></br></br>`);
+				return $color;
 			}
 		}
 }
 
-roulette(12);
+roulette(14);
 
+let number = prompt('Insert a COLOR');
+const predictor = (number) => {
+	
+}

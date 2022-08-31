869c571db1dda7a428115d94f9675cb391e1bada
Mauro Cordoba <mauro61816@gmail.com>
2022-08-31 20:32:14 -0300

    e-comerce Javascript
    Concepts appendChild, documentFragment, querySelector,

--- e-comerce.js
+++ e-comerce.js
@@ -1,7 +1,28 @@
-valuesKey = (name, code, price) =>{
-    img = (`<img href="https://www.pngwing.com/es/free-png-dkopa">`)
-    name = (`Name: ${name}`);
-    code = (`Code: ${code}`);
-    price = (`Price: ${price} U$s`);
-    return [name, code, price];
+const main = document.querySelector(".conteiner-flex");
+
+const valuesKey = (name, code, price) =>{
+    img = (`<img class="img-key" src="/key.png">`)
+    name = (`<h4>${name}</h4>`);
+    code = (`<span>${code}</span>`);
+    price = (`<span>Price:${price}U$s</span>`);
+    return [img, name, code, price];
 }
+
+let documentFragment = document.createDocumentFragment();
+
+const changeValue = (number) => {
+    document.querySelector(".key-code").value = number
+}
+
+for (let i = 1; i < 12; i++) {
+    let $code = Math.round(Math.random() * 1000 );
+    let $price = Math.round(Math.random() * 100 );
+    let key = valuesKey(`Name: ${i}`,`Code: ${$code}`, $price);
+    let div = document.createElement("DIV");
+    div.tabIndex = i;
+    div.classList.add(`item${i}`,'flex-item')
+    div.innerHTML = key[0] + key[1] + key[2] + key[3];
+    documentFragment.appendChild(div);
+}
+
+main.appendChild(documentFragment);

7ef8cce030f8610c88ffb5089ccb28aa10e99004
Mauro Cordoba <mauro61816@gmail.com>
2022-08-31 20:57:21 -0300

    e-comerce Javascript
    Adding eventListener click and css

--- e-comerce.js
+++ e-comerce.js
@@ -11,7 +11,7 @@ const valuesKey = (name, code, price) =>{
 let documentFragment = document.createDocumentFragment();
 
 const changeValue = (number) => {
-    document.querySelector(".key-code").value = number
+    document.querySelector(".key-code").value = number;
 }
 
 for (let i = 1; i < 12; i++) {
@@ -19,6 +19,7 @@ for (let i = 1; i < 12; i++) {
     let $price = Math.round(Math.random() * 100 );
     let key = valuesKey(`Name: ${i}`,`Code: ${$code}`, $price);
     let div = document.createElement("DIV");
+    div.addEventListener("click",() =>{changeValue($code)})
     div.tabIndex = i;
     div.classList.add(`item${i}`,'flex-item')
     div.innerHTML = key[0] + key[1] + key[2] + key[3];
